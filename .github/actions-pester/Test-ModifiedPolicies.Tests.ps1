059f23b07fafc72f02de18230149a4cb411feefc
Zach Trocinski <ztrocinski@outlook.com>
2023-02-27 15:50:42 -0600

    Additional logging

--- .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
+++ .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
@@ -12,14 +12,23 @@ Describe 'UnitTest-ModifiedPolicies' {
 
             $ModifiedPolicies | ForEach-Object {
                 $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                
-                $policyJson.properties.metadata.version | Should -Be '1.0.0'
-                
-                $policyJson.properties.metadata.category | Should -Not -BeNullOrEmpty
-                
-                $policyJson.properties.metadata.source | Should -BeExactly 'https://github.com/Azure/Enterprise-Scale/'
+                $policyFile = Split-Path $_ -leaf
+
+                $policyMetadataVersion = $policyJson.properties.metadata.version
+                Write-Warning "$($policyFile) - These are the environments: $($policyMetadataVersion)"
+                $policyMetadataVersion | Should -Be '1.0.0'
+
+                $policyMetadataCategories = $policyJson.properties.metadata.category
+                Write-Warning "$($policyFile) - These are the policy metadata categories: $($policyMetadataCategories)"
+                $policyMetadataCategories| Should -Not -BeNullOrEmpty
+
+                $policyMetadataSource = $policyJson.properties.metadata.source
+                Write-Warning "$($policyFile) - These is the policy source link: $($policyMetadataSource)"
+                $policyMetadataSource | Should -BeExactly 'https://github.com/Azure/Enterprise-Scale/'
                 
                 $alzEnvironments = @("AzureCloud", "AzureChinaCloud", "AzureUSGovernment")
+                $policyEnvironments = $policyJson.properties.metadata.alzCloudEnvironments
+                Write-Warning "$($policyFile) - These are the environments: $($policyEnvironments)"
                 $policyJson.properties.metadata.alzCloudEnvironments | Should -BeIn $alzEnvironments
             }
         }
@@ -36,7 +45,7 @@ Describe 'UnitTest-ModifiedPolicies' {
                 if ($policyParameters | Get-Member -MemberType NoteProperty)
                 {
                     $parameters = $policyParameters | Get-Member -MemberType NoteProperty | Select-Object -Expand Name
-                    Write-Warning "$($policyFile): These are the params: $($parameters)"
+                    Write-Warning "$($policyFile) - These are the params: $($parameters)"
                     $parameters = $policyParameters | Get-Member -MemberType NoteProperty
                     $parameters | ForEach-Object {
                         $key = $_.name
