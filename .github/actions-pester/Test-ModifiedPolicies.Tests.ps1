27f56c355ae065810b88688eda28cf4061deb243
Zach Trocinski <ztrocinski@outlook.com>
2023-02-27 23:23:39 -0600

    Add loops back

--- .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
+++ .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
@@ -4,60 +4,77 @@ BeforeAll {
 
     $ModifiedPolicies = Get-ModifiedPolicies -Verbose 
     Write-Warning "These are the modified policies: $($ModifiedPolicies)"
-    
-    $ModifiedPolicies | ForEach-Object {
-        $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-        $policyFile = Split-Path $_ -Leaf
-    }
-
-    git checkout policy-unittests
-    $ModifiedPolicies | ForEach-Object {
-        $policyJsonMain = Get-Content -Path $_ -Raw | ConvertFrom-Json
-    }
 }
 
 Describe 'UnitTest-ModifiedPolicies' {
     Context "Validate policy metadata" {
         It "Check for valid metadata version" {
-            $policyMetadataVersion = $policyJson.properties.metadata.version
-            Write-Warning "$($policyFile) - This is the policy metadata version from the pr branch: $($policyMetadataVersion)"
-
-            $policyMetadataVersionMainBranch = $policyJsonMain.properties.metadata.version
-            Write-Warning "$($policyFile) - This is the policy metadata version from the main branch: $($policyMetadataVersionMainBranch)"
+            $ModifiedPolicies | ForEach-Object {
+                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $policyFile = Split-Path $_ -Leaf
+                $policyMetadataVersion = $policyJson.properties.metadata.version
+                Write-Warning "$($policyFile) - This is the policy metadata version from the PR branch: $($policyMetadataVersion)"
+            }
+        
+            git checkout policy-unittests
+            $ModifiedPolicies | ForEach-Object {
+                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $policyFile = Split-Path $_ -Leaf
+                $policyJsonMain = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $policyMetadataVersionMainBranch = $policyJsonMain.properties.metadata.version
+                Write-Warning "$($policyFile) - This is the policy metadata version from the main branch: $($policyMetadataVersionMainBranch)"
+            }
         }
 
         It "Check policy metadata categories" {
-            $policyMetadataCategories = $policyJson.properties.metadata.category
-            Write-Warning "$($policyFile) - These are the policy metadata categories: $($policyMetadataCategories)"
-            $policyMetadataCategories | Should -Not -BeNullOrEmpty
+            $ModifiedPolicies | ForEach-Object {
+                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $policyFile = Split-Path $_ -Leaf
+                $policyMetadataCategories = $policyJson.properties.metadata.category
+                Write-Warning "$($policyFile) - These are the policy metadata categories: $($policyMetadataCategories)"
+                $policyMetadataCategories | Should -Not -BeNullOrEmpty
+            }
+            
         }
 
         It "Check policy metadata source" {
-            $policyMetadataSource = $policyJson.properties.metadata.source
-            Write-Warning "$($policyFile) - This is the policy source link: $($policyMetadataSource)"
-            $policyMetadataSource | Should -BeExactly 'https://github.com/Azure/Enterprise-Scale/'
+            $ModifiedPolicies | ForEach-Object {
+                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $policyFile = Split-Path $_ -Leaf
+                $policyMetadataSource = $policyJson.properties.metadata.source
+                Write-Warning "$($policyFile) - This is the policy source link: $($policyMetadataSource)"
+                $policyMetadataSource | Should -BeExactly 'https://github.com/Azure/Enterprise-Scale/'
+            }
         }
         
         It "Check policy metadata alzenvironments" {
-            $alzEnvironments = @("AzureCloud", "AzureChinaCloud", "AzureUSGovernment")
-            $policyEnvironments = $policyJson.properties.metadata.alzCloudEnvironments
-            Write-Warning "$($policyFile) - These are the environments: $($policyEnvironments)"
-            $policyJson.properties.metadata.alzCloudEnvironments | Should -BeIn $alzEnvironments
+            $ModifiedPolicies | ForEach-Object {
+                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $policyFile = Split-Path $_ -Leaf
+                $alzEnvironments = @("AzureCloud", "AzureChinaCloud", "AzureUSGovernment")
+                $policyEnvironments = $policyJson.properties.metadata.alzCloudEnvironments
+                Write-Warning "$($policyFile) - These are the environments: $($policyEnvironments)"
+                $policyJson.properties.metadata.alzCloudEnvironments | Should -BeIn $alzEnvironments
+            }
         }
     }
     Context "Validate policy parameters" {
         It 'Check for parameter default values' {
-            $policyParameters = $policyJson.properties.parameters
-            if ($policyParameters | Get-Member -MemberType NoteProperty)
-            {
-                $parameters = $policyParameters | Get-Member -MemberType NoteProperty | Select-Object -Expand Name
-                Write-Warning "$($policyFile) - These are the params: $($parameters)"
-                $parameters = $policyParameters | Get-Member -MemberType NoteProperty
-                $parameters | ForEach-Object {
-                    $key = $_.name
-                    $defaultValue = $policyParameters.$key | Get-Member -MemberType NoteProperty | Where-Object Name -EQ "defaultValue"
-                    Write-Warning "$($policyFile) - Parameter: $($key) - Default Value: $($defaultValue)"
-                    $policyParameters.$key.defaultValue | Should -Not -BeNullOrEmpty
+            $ModifiedPolicies | ForEach-Object {
+                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $policyFile = Split-Path $_ -Leaf
+                $policyParameters = $policyJson.properties.parameters
+                if ($policyParameters | Get-Member -MemberType NoteProperty)
+                {
+                    $parameters = $policyParameters | Get-Member -MemberType NoteProperty | Select-Object -Expand Name
+                    Write-Warning "$($policyFile) - These are the params: $($parameters)"
+                    $parameters = $policyParameters | Get-Member -MemberType NoteProperty
+                    $parameters | ForEach-Object {
+                        $key = $_.name
+                        $defaultValue = $policyParameters.$key | Get-Member -MemberType NoteProperty | Where-Object Name -EQ "defaultValue"
+                        Write-Warning "$($policyFile) - Parameter: $($key) - Default Value: $($defaultValue)"
+                        $policyParameters.$key.defaultValue | Should -Not -BeNullOrEmpty
+                    }
                 }
             }
         }
