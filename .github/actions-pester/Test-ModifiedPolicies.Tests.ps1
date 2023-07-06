b65b746a32492299b2f2a017eabc4064f9ff6d37
Zach Trocinski <ztrocinski@outlook.com>
2023-07-05 21:20:02 -0500

    Updated pester tests to include modified and added files and fix issues with metadata version ref.

--- .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
+++ .github/actions-pester/Test-ModifiedPolicies.Tests.ps1
@@ -2,90 +2,109 @@ Describe 'UnitTest-ModifiedPolicies' {
     BeforeAll {
         Import-Module -Name $PSScriptRoot\PolicyPesterTestHelper.psm1 -Force -Verbose
 
-        $ModifiedPolicies = Get-ModifiedPolicies -Verbose
-        Write-Warning "These are the modified policies: $($ModifiedPolicies)"
+        $ModifiedFiles = Get-PolicyFiles -DiffFilter "M"
+        if ($ModifiedFiles -ne $null)
+        {
+            Write-Warning "These are the modified policies: $($ModifiedFiles)"
+        }
+        else
+        {
+            Write-Warning "There are no modified policies"
+        }
+
+        $AddedFiles = Get-PolicyFiles -DiffFilter "A"
+        if ($AddedFiles -ne $null)
+        {
+            Write-Warning "These are the added policies: $($AddedFiles)"
+        }
+        else
+        {
+            Write-Warning "There are no added policies"
+        }
+
+        $ModifiedAddedFiles = $ModifiedFiles + $AddedFiles
     }
+
+
 
     Context "Validate policy metadata" {
 
         It "Check policy metadata version exists" {
-            $ModifiedPolicies | ForEach-Object {
-                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                $policyFile = Split-Path $_ -Leaf
-                $policyMetadataVersion = $policyJson.properties.metadata.version
-                Write-Warning "$($policyFile) - The current metadata version for the PR branch is : $($policyMetadataVersion)"
-                $policyMetadataVersion | Should -Not -BeNullOrEmpty
-
+            $ModifiedAddedFiles | ForEach-Object {
+                $PolicyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $PolicyFile = Split-Path $_ -Leaf
+                $PolicyMetadataVersion = $PolicyJson.properties.metadata.version
+                Write-Warning "$($PolicyFile) - The current metadata version for the PR branch is : $($PolicyMetadataVersion)"
+                $PolicyMetadataVersion | Should -Not -BeNullOrEmpty
             }
         }
 
-        It "Check policy metadata version is greater than its previous version" {
-            $ModifiedPolicies | ForEach-Object {
-                $policyFile = Split-Path $_ -Leaf
-                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                $previousPolicyDefinitionRawUrl = "https://raw.githubusercontent.com/Azure/Enterprise-Scale/main/$_"
-                $previousPolicyDefinitionOutputFile = "./previous-$policyFile"
-                Invoke-WebRequest -Uri $previousPolicyDefinitionRawUrl -OutFile $previousPolicyDefinitionOutputFile
-                $PreviousPolicyDefinitionsFile = Get-Content $previousPolicyDefinitionOutputFile -Raw | ConvertFrom-Json
+        It "Check policy metadata version is greater than its previous version" -Skip:($ModifiedFiles -ne $null) {
+            $ModifiedFiles | ForEach-Object {
+                $PolicyFile = Split-Path $_ -Leaf
+                $PolicyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $PreviousPolicyDefinitionRawUrl = "https://raw.githubusercontent.com/Azure/Enterprise-Scale/main/$_"
+                $PreviousPolicyDefinitionOutputFile = "./previous-$PolicyFile"
+                Invoke-WebRequest -Uri $PreviousPolicyDefinitionRawUrl -OutFile $PreviousPolicyDefinitionOutputFile
+                $PreviousPolicyDefinitionsFile = Get-Content $PreviousPolicyDefinitionOutputFile -Raw | ConvertFrom-Json
                 $PreviousPolicyDefinitionsFileVersion = $PreviousPolicyDefinitionsFile.properties.metadata.version
-                Write-Warning "$($policyFile) - The current metadata version for the main branch is : $($PreviousPolicyDefinitionsFileVersion)"
-                $policyMetadataVersion = $policyJson.properties.metadata.version
-                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                $policyMetadataVersion | Should -BeGreaterThan $PreviousPolicyDefinitionsFileVersion
-
+                Write-Warning "$($PolicyFile) - The current metadata version for the main branch is : $($PreviousPolicyDefinitionsFileVersion)"
+                $PolicyMetadataVersion = $PolicyJson.properties.metadata.version
+                $PolicyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $PolicyMetadataVersion | Should -BeGreaterThan $PreviousPolicyDefinitionsFileVersion
             }
         }
 
         It "Check policy metadata category exists" {
-            $ModifiedPolicies | ForEach-Object {
-                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                $policyFile = Split-Path $_ -Leaf
-                $policyMetadataCategories = $policyJson.properties.metadata.category
-                Write-Warning "$($policyFile) - These are the policy metadata categories: $($policyMetadataCategories)"
-                $policyMetadataCategories | Should -Not -BeNullOrEmpty
+            $ModifiedAddedFiles | ForEach-Object {
+                $PolicyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $PolicyFile = Split-Path $_ -Leaf
+                $PolicyMetadataCategories = $PolicyJson.properties.metadata.category
+                Write-Warning "$($PolicyFile) - These are the policy metadata categories: $($PolicyMetadataCategories)"
+                $PolicyMetadataCategories | Should -Not -BeNullOrEmpty
             }
         }
 
         It "Check policy metadata source is set to Enterprise-Scale repo" {
-            $ModifiedPolicies | ForEach-Object {
-                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                $policyFile = Split-Path $_ -Leaf
-                $policyMetadataSource = $policyJson.properties.metadata.source
-                Write-Warning "$($policyFile) - This is the policy source link: $($policyMetadataSource)"
-                $policyMetadataSource | Should -Be 'https://github.com/Azure/Enterprise-Scale/'
+            $ModifiedAddedFiles | ForEach-Object {
+                $PolicyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $PolicyFile = Split-Path $_ -Leaf
+                $PolicyMetadataSource = $PolicyJson.properties.metadata.source
+                Write-Warning "$($PolicyFile) - This is the policy source link: $($PolicyMetadataSource)"
+                $PolicyMetadataSource | Should -Be 'https://github.com/Azure/Enterprise-Scale/'
             }
         }
 
         It "Check policy metadata ALZ Environments are specified for Public, US Gov or China Clouds" {
-            $ModifiedPolicies | ForEach-Object {
-                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                $policyFile = Split-Path $_ -Leaf
-                $alzEnvironments = @("AzureCloud", "AzureChinaCloud", "AzureUSGovernment")
-                $policyEnvironments = $policyJson.properties.metadata.alzCloudEnvironments
-                Write-Warning "$($policyFile) - These are the environments: $($policyEnvironments)"
-                $policyJson.properties.metadata.alzCloudEnvironments | Should -BeIn $alzEnvironments
+            $ModifiedAddedFiles | ForEach-Object {
+                $PolicyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                $PolicyFile = Split-Path $_ -Leaf
+                $AlzEnvironments = @("AzureCloud", "AzureChinaCloud", "AzureUSGovernment")
+                $PolicyEnvironments = $PolicyJson.properties.metadata.alzCloudEnvironments
+                Write-Warning "$($PolicyFile) - These are the environments: $($PolicyEnvironments)"
+                $PolicyJson.properties.metadata.alzCloudEnvironments | Should -BeIn $AlzEnvironments
             }
         }
-    }
-    Context "Validate policy parameters" {
-        It 'Check for policy parameters have default values' {
-            $ModifiedPolicies | ForEach-Object {
-                $policyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
-                $policyFile = Split-Path $_ -Leaf
-                $policyParameters = $policyJson.properties.parameters
-                if ($policyParameters | Get-Member -MemberType NoteProperty)
-                {
-                    $parameters = $policyParameters | Get-Member -MemberType NoteProperty | Select-Object -Expand Name
-                    Write-Warning "$($policyFile) - These are the params: $($parameters)"
-                    $parameters = $policyParameters | Get-Member -MemberType NoteProperty
-                    $parameters | ForEach-Object {
-                        $key = $_.name
-                        $defaultValue = $policyParameters.$key | Get-Member -MemberType NoteProperty | Where-Object Name -EQ "defaultValue"
-                        Write-Warning "$($policyFile) - Parameter: $($key) - Default Value: $($defaultValue)"
-                        $policyParameters.$key.defaultValue | Should -Not -BeNullOrEmpty
+        }
+        Context "Validate policy parameters" {
+            It 'Check for policy parameters have default values' {
+                $ModifiedAddedFiles | ForEach-Object {
+                    $PolicyJson = Get-Content -Path $_ -Raw | ConvertFrom-Json
+                    $PolicyFile = Split-Path $_ -Leaf
+                    $PolicyParameters = $PolicyJson.properties.parameters
+                    if ($PolicyParameters | Get-Member -MemberType NoteProperty)
+                    {
+                        $Parameters = $PolicyParameters | Get-Member -MemberType NoteProperty | Select-Object -Expand Name
+                        Write-Warning "$($PolicyFile) - These are the params: $($Parameters)"
+                        $Parameters = $PolicyParameters | Get-Member -MemberType NoteProperty
+                        $Parameters | ForEach-Object {
+                            $key = $_.name
+                            $defaultValue = $PolicyParameters.$key | Get-Member -MemberType NoteProperty | Where-Object Name -EQ "defaultValue"
+                            Write-Warning "$($PolicyFile) - Parameter: $($key) - Default Value: $($defaultValue)"
+                            $PolicyParameters.$key.defaultValue | Should -Not -BeNullOrEmpty
+                        }
                     }
                 }
             }
         }
     }
-}
