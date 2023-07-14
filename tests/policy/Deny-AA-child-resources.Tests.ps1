dc2fbb7e49cb7fce4048e01015f8f08e59920735
Sacha Narinx <Springstone@users.noreply.github.com>
2023-07-14 17:56:23 +0400

    .

--- tests/policy/Deny-AA-child-resources.Tests.ps1
+++ tests/policy/Deny-AA-child-resources.Tests.ps1
@@ -33,7 +33,7 @@ Describe "Testing policy 'Deny-AA-child-resources'" -Tag "deny-automation-childr
 
     Context "Test adding child resources on Automation Account when created or updated" -Tag "deny-automation-children" {
 
-        It "Should deny non-compliant Automation Account - Runbook" -Tag "deny-noncompliant-automation" {
+        It "Should allow compliant Automation Account" -Tag "deny-noncompliant-automation" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
@@ -43,37 +43,110 @@ Describe "Testing policy 'Deny-AA-child-resources'" -Tag "deny-automation-childr
                        -Name "ContosoAA001" `
                        -Location "uksouth" `
                        -DisablePublicNetworkAccess
-
-                    New-AzAutomationRunbook `
-                          -ResourceGroupName $ResourceGroup.ResourceGroupName `
-                          -AutomationAccountName $aa.AutomationAccountName `
-                          -Name "ContosoRunbook001"
                        
-               } | Should -Throw "*disallowed by policy*"
+               } | Should -Not -Throw
             }
         }
+        
+        # It "Should deny non-compliant Automation Account - Runbook" -Tag "deny-noncompliant-automation" {
+        #     AzTest -ResourceGroup {
+        #         param($ResourceGroup)
 
-        It "Should deny non-compliant Automation Account - Variable" -Tag "deny-noncompliant-automation" {
+        #         {
+        #             $aa = New-AzAutomationAccount `
+        #                -ResourceGroupName $ResourceGroup.ResourceGroupName `
+        #                -Name "ContosoAA001" `
+        #                -Location "uksouth" `
+        #                -DisablePublicNetworkAccess
+
+        #             New-AzAutomationRunbook `
+        #                   -ResourceGroupName $ResourceGroup.ResourceGroupName `
+        #                   -AutomationAccountName $aa.AutomationAccountName `
+        #                   -Name "ContosoRunbook001"
+                       
+        #        } | Should -Throw "*disallowed by policy*"
+        #     }
+        # }
+
+        It "Should deny non-compliant Automation Account - Runbook - via API" -Tag "deny-noncompliant-automation" {
             AzTest -ResourceGroup {
                 param($ResourceGroup)
 
-                {
-                    New-AzAutomationAccount `
-                       -ResourceGroupName $ResourceGroup.ResourceGroupName `
-                       -Name "ContosoAA002" `
-                       -Location "uksouth" `
-                       -DisablePublicNetworkAccess
+                $object = @{
+                    properties = @{
+                        publicNetworkAccess = $false
+                    }
+                    location = "uksouth"
+                }
+                $payload = ConvertTo-Json -InputObject $object -Depth 100
 
-                    New-AzAutomationRunbook `
-                          -ResourceGroupName $ResourceGroup.ResourceGroupName `
-                          -AutomationAccountName "ContosoAA002" `
-                          -Name "ContosoVariable001" `
-                          -Encrypted $False `
-                          -Value "My String"
-                       
-               } | Should -Throw "*disallowed by policy*"
+                # Should be disallowed by policy, so exception should be thrown.
+                {
+                    $httpResponse = Invoke-AzRestMethod `
+                        -ResourceGroupName $ResourceGroup.ResourceGroupName `
+                        -ResourceProviderName "Microsoft.Automation" `
+                        -ResourceType "automationAccounts" `
+                        -Name "ContosoAA001" `
+                        -ApiVersion "2021-06-22" `
+                        -Method "PUT" `
+                        -Payload $payload
+            
+                    if ($httpResponse.StatusCode -eq 200 -or $httpResponse.StatusCode -eq 201) {
+                        # Automation Account created
+                    }
+                    else {
+                        throw "Operation failed with message: '$($httpResponse.Content)'"
+                    }              
+
+                    $object = @{
+                        properties = @{
+                            runbookType = $false
+                        }
+                    }
+                    $payload = ConvertTo-Json -InputObject $object -Depth 100
+
+                    $httpResponse = Invoke-AzRestMethod `
+                        -ResourceGroupName $ResourceGroup.ResourceGroupName `
+                        -ResourceProviderName "Microsoft.Automation" `
+                        -ResourceType "runbooks" `
+                        -AutomationAccountName "ContosoAA001" `
+                        -Name "ContosoRunbook001" `
+                        -ApiVersion "2019-06-01" `
+                        -Method "PUT" `
+                        -Payload $payload
+            
+                    if ($httpResponse.StatusCode -eq 200 -or $httpResponse.StatusCode -eq 201) {
+                        # Automation Account - Runbook created
+                    }
+                    else {
+                        throw "Operation failed with message: '$($httpResponse.Content)'"
+                    }
+
+                } | Should -Throw "*disallowed by policy*"
             }
         }
+
+        # It "Should deny non-compliant Automation Account - Variable" -Tag "deny-noncompliant-automation" {
+        #     AzTest -ResourceGroup {
+        #         param($ResourceGroup)
+
+        #         {
+        #             New-AzAutomationAccount `
+        #                -ResourceGroupName $ResourceGroup.ResourceGroupName `
+        #                -Name "ContosoAA002" `
+        #                -Location "uksouth" `
+        #                -DisablePublicNetworkAccess
+
+        #             New-AzAutomationRunbook `
+        #                   -ResourceGroupName $ResourceGroup.ResourceGroupName `
+        #                   -AutomationAccountName "ContosoAA002" `
+        #                   -Name "ContosoVariable001" `
+        #                   -Encrypted $False `
+        #                   -Value "My String"
+                       
+        #        } | Should -Throw "*disallowed by policy*"
+        #     }
+        # }
     }
 
 }
