0304deb125daa56afa6aae6a90ac6dddb7fb71ee
Sacha Narinx <Springstone@users.noreply.github.com>
2024-01-17 16:48:23 +0400

    .

--- .github/actions-pester/PTF-ModifiedPolicies.ps1
+++ .github/actions-pester/PTF-ModifiedPolicies.ps1
@@ -22,32 +22,30 @@ function RunPester
     exit $result.FailedCount
 }
 
-# $ModifiedFiles = @(Get-PolicyFiles -DiffFilter "M")
-# if ($null -ne $ModifiedFiles)
-# {
-#     Write-Warning "These are the modified policies: $($ModifiedFiles)"
-# }
-# else
-# {
-#     Write-Warning "There are no modified policies"
-# }
+$ModifiedFiles = @(Get-PolicyFiles -DiffFilter "M")
+if ($null -ne $ModifiedFiles)
+{
+    Write-Warning "These are the modified policies: $($ModifiedFiles)"
+}
+else
+{
+    Write-Warning "There are no modified policies"
+}
 
-# $AddedFiles = @(Get-PolicyFiles -DiffFilter "A")
-# if ($null -ne $AddedFiles)
-# {
-#     Write-Warning "These are the added policies: $($AddedFiles)"
-# }
-# else
-# {
-#     Write-Warning "There are no added policies"
-# }
+$AddedFiles = @(Get-PolicyFiles -DiffFilter "A")
+if ($null -ne $AddedFiles)
+{
+    Write-Warning "These are the added policies: $($AddedFiles)"
+}
+else
+{
+    Write-Warning "There are no added policies"
+}
 
-# $ModifiedAddedFiles = $ModifiedFiles + $AddedFiles
+$ModifiedAddedFiles = $ModifiedFiles + $AddedFiles
 
-$PRBranch = "$($env:GITHUB_HEAD_REF)"
-# $PRBranch = "PolicyTestingUpdate"
-$ModifiedAddedFiles = @(git diff --diff-filter=$DiffFilter --name-only origin/main $PRBranch -- $policydirectory)
-# $ModifiedAddedFiles = Get-Item "$($env:POLICY_DIR)/*.json"
+# $PRBranch = "$($env:GITHUB_HEAD_REF)"
+# $ModifiedAddedFiles = @(git diff --diff-filter=$DiffFilter --name-only origin/main $PRBranch -- $policydirectory)
 
 $ModifiedAddedFiles | ForEach-Object {
 
