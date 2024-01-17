4f4fd29c89bbdef1f0ba538d85d4856b79a331d2
Sacha Narinx <Springstone@users.noreply.github.com>
2024-01-17 11:53:00 +0400

    Changing when Policy Tests run

---
+++ .github/actions-pester/PTF-ModifiedPolicies.Tests.ps1
@@ -0,0 +1,53 @@
+Describe 'Policy Testing Framework' {
+    BeforeAll {
+        Import-Module -Name $PSScriptRoot\PolicyPesterTestHelper.psm1 -Force -Verbose
+
+        $ModifiedFiles = @(Get-PolicyFiles -DiffFilter "M")
+        if ($ModifiedFiles -ne $null)
+        {
+            Write-Warning "These are the modified policies: $($ModifiedFiles)"
+        }
+        else
+        {
+            Write-Warning "There are no modified policies"
+        }
+
+        $AddedFiles = @(Get-PolicyFiles -DiffFilter "A")
+        if ($AddedFiles -ne $null)
+        {
+            Write-Warning "These are the added policies: $($AddedFiles)"
+        }
+        else
+        {
+            Write-Warning "There are no added policies"
+        }
+
+        $ModifiedAddedFiles = $ModifiedFiles + $AddedFiles
+    }
+
+    Context "Test Changed Policies" {
+        
+        It 'Run pester tests on changed policies' {
+
+            If ($ModifiedAddedFiles -eq $null)
+            {
+                Write-Warning "There are no modified or added policies"
+            }
+            else
+            {
+                $ModifiedAddedFiles | ForEach-Object {
+
+                    $PolicyFile = Split-Path $_ -Leaf
+                    $PolicyFileClean = $PolicyFile -replace ".json", ""
+
+                    $testPath = "tests/" + $PolicyFileClean + ".Tests.ps1"
+
+                    Write-Warning "Running pester tests on $PolicyFileClean"
+                    
+                    Invoke-Pester -Script $testPath -PassThru
+                }
+            }
+        }
+
+    }
+}
